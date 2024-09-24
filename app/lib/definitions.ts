53fbee7d62fae5a8cd7dbd84ecb0a173f9a87ffd
Sandro.dev <sandrosuarezaranda@gmail.com>
2024-09-24 18:49:52 +0200

    Type Email deleted

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -1,29 +1,24 @@
-
 export type CustomerData = {
   username: string;
   email: string;
   password: string;
   image_url: string;
-};   
+};
 
 export type FormFieldProps = {
   errors: ErrorState;
   setErrors: React.Dispatch<React.SetStateAction<ErrorState>>;
 };
 
- export type ErrorState = {
+export type ErrorState = {
   userErrors: string[];
   passwordErrors: string[];
   confirmPasswordErrors: string[];
   emailErrors: string[];
   imageErrors: string[];
-}
+};
 
 export type EyeIconProps = {
   showPassword: boolean;
   setShowPassword: React.Dispatch<React.SetStateAction<boolean>>;
 };
-
-export type Email = {
-  email:string
-}
