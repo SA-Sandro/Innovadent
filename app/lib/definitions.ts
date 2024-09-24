c3fa28e18fd1d631cea69c94f5219eb84cbdc802
Sandro.dev <sandrosuarezaranda@gmail.com>
2024-09-24 10:02:35 +0200

    New types added

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -1,7 +1,25 @@
-export type Customer = {
-  id: string;
-  name: string;
+
+export type CustomerData = {
+  username: string;
   email: string;
+  password: string;
   image_url: string;
-  password:string
+};   
+
+export type FormFieldProps = {
+  errors: ErrorState;
+  setErrors: React.Dispatch<React.SetStateAction<ErrorState>>;
+};
+
+ export type ErrorState = {
+  userErrors: string[];
+  passwordErrors: string[];
+  confirmPasswordErrors: string[];
+  emailErrors: string[];
+  imageErrors: string[];
+}
+
+export type EyeIconProps = {
+  showPassword: boolean;
+  setShowPassword: React.Dispatch<React.SetStateAction<boolean>>;
 };
