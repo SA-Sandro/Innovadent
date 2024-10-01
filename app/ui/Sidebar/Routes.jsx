5d0f887fb7fe576cd58af7c7c3c66f43e4c1c5d0
Sandro.dev <sandrosuarezaranda@gmail.com>
2024-10-01 18:22:32 +0200

    aside links were componetizated

---
+++ app/ui/Sidebar/Routes.jsx
@@ -0,0 +1,72 @@
+"use client";
+
+import { CiHome, CiLogin } from "react-icons/ci";
+import { LuUserCircle2 } from "react-icons/lu";
+import { MdOutlineManageAccounts } from "react-icons/md";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function Routes() {
+  const pathName = usePathname();
+
+  return (
+    <div className="px-3 py-4 bg-[#1F2937] h-full">
+      <div className="flex flex-col items-center mt-5 cursor-pointer">
+        <Link href="/">
+          <Image
+            width={60}
+            height={60}
+            priority={true}
+            src="/images/logo_empresa.png"
+            alt="Logo de Innovadent"
+            className="w-auto h-auto"
+          />
+        </Link>
+        <h3 className="text-white font-bold text-2xl">Innovadent</h3>
+      </div>
+      <div className="flex flex-col text-white h-[70%] justify-between">
+        <div className="mt-10 flex flex-col space-y-3 text-md text-left ">
+          <Link
+            href="/"
+            className={`flex items-center hover:bg-gray-700 rounded-md p-1 ${
+              pathName === "/" ? "bg-blue-950" : ""
+            }`}
+          >
+            <CiHome size={20} strokeWidth={0.9} />
+            <span className="ml-1">Inicio</span>
+          </Link>
+          <Link
+            href="/create-appointment"
+            className={`flex items-center hover:bg-gray-700 rounded-md p-1 ${
+              pathName === "/create-appointment" ? "bg-blue-950" : ""
+            }`}
+          >
+            <MdOutlineManageAccounts size={20} />
+            <span className="ml-1">Gestor de citas</span>
+          </Link>
+        </div>
+        <div className="space-y-2">
+          <Link
+            href="/login"
+            className={`flex items-center hover:bg-gray-700 rounded-md p-1 ${
+              pathName === "/login" ? "bg-blue-950" : ""
+            }`}
+          >
+            <CiLogin size={20} strokeWidth={1} />
+            <span className="ml-2">Iniciar sesión</span>
+          </Link>
+          <Link
+            href="/register"
+            className={`flex items-center hover:bg-gray-700 rounded-md p-1 ${
+              pathName === "/register" ? "bg-blue-950" : ""
+            }`}
+          >
+            <LuUserCircle2 size={20} />
+            <span className="ml-2">Regístrate</span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
