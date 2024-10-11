7633b26d9776ed777064fbba9629d816a7a701dc
Sandro.dev <sandrosuarezaranda@gmail.com>
2024-10-11 16:37:24 +0200

    Add the SuccessAppointment component. Add id attribute to inputs and add a loader

--- app/create-appointment/page.tsx
+++ app/create-appointment/page.tsx
@@ -3,15 +3,17 @@
 import { createAppointment } from "@/lib/actions";
 import { getBookedHourByDate } from "@/lib/data";
 import { appointmentInitialState } from "@/lib/definitions";
-import { ChangeEvent, useState } from "react"
+import Modal from "@/lib/modal";
+import SuccessAppointment from "@/ui/modal/SuccessAppointment";
+import SubmitButton from "@/ui/SubmitButton";
+import { ChangeEvent, useEffect, useState } from "react"
 import { useFormState } from "react-dom";
-import { MdOutlineEventBusy } from "react-icons/md";
 
 export default function Appointment() {
-
   const [selectValue, setSelectValue] = useState<string>();
   const [state, action] = useFormState(createAppointment, appointmentInitialState);
-  const [bookedHours, setBookedHours] = useState<Array<string>>([]);  // Inicialmente vacío
+  const [bookedHours, setBookedHours] = useState<Array<string>>([]);
+
   const AVAILABLE_HOURS = [
     '09:00:00', '10:00:00', '11:00:00', '12:00:00', '13:00:00', '14:00:00',
     '15:00:00', '16:00:00', '17:00:00'
@@ -19,27 +21,38 @@ export default function Appointment() {
 
   const showBookedHours = async (event: ChangeEvent<HTMLInputElement>) => {
     const selectedDate = event.target.value;
-
     const result = await getBookedHourByDate(new Date(selectedDate));
-    if (!result) {
-      setBookedHours([]);
-      return;
-    }
-
-    setBookedHours(result);
+    setBookedHours(result || []);
   };
+
+  useEffect(() => {
+    if (state.isSuccess) {
+      new Modal().showModal();
+      clearForm();
+    }
+  }, [state.isSuccess])
+
+  const clearForm = () => {
+    const motiveSelect = document.getElementById('motive') as HTMLSelectElement;
+    motiveSelect.selectedIndex = 0;
+
+    const date = document.getElementById('date') as HTMLInputElement;
+    date.value = '';
+
+    const hour = document.getElementById('hour') as HTMLSelectElement;
+    hour.selectedIndex = 0;
+  }
 
   return (
     <div className="relative flex justify-center items-center bg-white p-10 rounded-xl w-[35%]">
       <div className="px-10 rounded-xl w-full">
         <h1 className="text-2xl font-bold text-center">Reservar cita</h1>
         <form action={action} className="space-y-5">
-
           <div>
             <label className="text-gray-800 text-sm mb-2 block">
               Elije el motivo de consulta <span className="text-red-500"> *</span>
             </label>
-            <select name="motive"
+            <select id="motive" name="motive"
               className="text-gray-800 bg-white border border-gray-300 w-full text-sm px-4 py-3 rounded-md outline-blue-500 cursor-pointer"
               onChange={(event: ChangeEvent<HTMLSelectElement>) => setSelectValue(event.target.value)}>
               <option value="">Seleccione una opción</option>
@@ -73,6 +86,7 @@ export default function Appointment() {
             <input
               onChange={showBookedHours}
               name="date"
+              id="date"
               type="date"
               className="cursor-pointer text-gray-800 bg-white border border-gray-300 w-full text-sm px-4 py-3 rounded-md outline-blue-500"
             />
@@ -88,11 +102,10 @@ export default function Appointment() {
             <label className="text-gray-800 text-sm mb-2 block">
               Hora <span className="text-red-500">*</span>
             </label>
-            <select name="hour" className="text-gray-800 bg-white border border-gray-300 w-full text-sm px-4 py-3 rounded-md outline-blue-500 cursor-pointer">
+            <select id="hour" name="hour" className="text-gray-800 bg-white border border-gray-300 w-full text-sm px-4 py-3 rounded-md outline-blue-500 cursor-pointer">
               {AVAILABLE_HOURS.map((hour) => (
                 <option key={hour} value={hour} className={bookedHours.includes(hour) ? 'bg-red-500 text-white flex' : ''}>
                   {hour.slice(0, 5)}
-                  {bookedHours.includes(hour) && <MdOutlineEventBusy />}
                 </option>
               ))}
             </select>
@@ -104,13 +117,10 @@ export default function Appointment() {
               </div>
             )}
           </div>
-          <div className="flex justify-center items-center">
-            <button type="submit" className="w-full py-3 px-4 text-sm tracking-wider font-semibold rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none">
-              Crear cita
-            </button>
-          </div>
+          <SubmitButton />
         </form>
       </div>
+      <SuccessAppointment />
     </div>
   );
 }
