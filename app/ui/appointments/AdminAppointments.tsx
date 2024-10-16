87f3492b98623279766e60e1ff18732cc69d88bb
Sandro.dev <sandrosuarezaranda@gmail.com>
2024-10-16 12:44:18 +0200

    Update the Image dimension

--- app/ui/appointments/AdminAppointments.tsx
+++ app/ui/appointments/AdminAppointments.tsx
@@ -3,11 +3,20 @@
 
 export default function AdminAppointment() {
 
+
+
+
     return (
         <div className="relative bg-white rounded-xl flex justify-center items-center py-10 px-52">
             <table className="table-auto" id="table">
                 <thead>
                     <tr className="space-x-10">
+                        <th scope="col" className="px-6 py-3">
+                            Usuario
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Correo
+                        </th>
                         <th scope="col" className="px-6 py-3">
                             Motivo de la consulta
                         </th>
@@ -18,10 +27,32 @@ export default function AdminAppointment() {
                             Hora
                         </th>
                         <th scope="col" className="px-6 py-3">
-                            Suspender cita
+                            Estado
                         </th>
                     </tr>
                 </thead>
+                <tbody className="border-b ">
+                    <tr className="bg-white border-b cursor-pointer hover:bg-gray-100">
+                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                            Sandro
+                        </th>
+                        <td className="px-6 py-4 text-center">
+                            sandrosuarezaranda@gmail.com
+                        </td>
+                        <td className="px-6 py-4">
+                            Motivo de la consulta
+                        </td>
+                        <td className="px-6 py-4 " >
+                            Fecha de la cita
+                        </td>
+                        <td className="px-6 py-4 " >
+                            Hora
+                        </td>
+                        <td className="px-6 py-4 " >
+                            Estado
+                        </td>
+                    </tr>
+                </tbody>
 
             </table>
         </div>
