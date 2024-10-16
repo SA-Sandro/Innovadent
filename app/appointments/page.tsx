d6f1e644949f520ac0e7431cb18ac9bca89a0b0d
Sandro.dev <sandrosuarezaranda@gmail.com>
2024-10-16 11:11:58 +0200

    Add functions to manage the data fetching and to delete the specified appointment

--- app/appointments/page.tsx
+++ app/appointments/page.tsx
@@ -1,16 +1,23 @@
 'use client';
 
 import { AppointmentData, AppointmentRows } from "@/lib/definitions";
+import Modal from "@/lib/modal";
 import { getPlainSession } from "@/lib/session";
+import ButtonLoader from "@/ui/ButtonLoader";
+import WarningAppointmentDelete from "@/ui/modal/WarningAppointmentDelete";
 import { useEffect, useState } from "react";
+import { CiSquareRemove } from "react-icons/ci";
 
 export default function Appointments() {
-    const [appointments, setAppointments] = useState<AppointmentRows | null>(null); // Initialize state with null
-    const [loading, setLoading] = useState<boolean>(true); // Loading state
+    const [appointments, setAppointments] = useState<AppointmentRows | null>(null);
+    const [isLoadingWhileFetching, setIsLoadingWhileFetching] = useState<boolean>(true);
+    const [isLoadingWhileDeleting, setIsLoadingWhileDeleting] = useState<boolean>(false);
+    const [selectedAppointmentId, setSelectedAppointmentId] = useState<string | undefined>(undefined);
 
     useEffect(() => {
         const fetchAppointments = async () => {
             try {
+                setIsLoadingWhileFetching(true);
                 const session = await getPlainSession();
                 const email = session?.email;
 
@@ -32,23 +39,48 @@ export default function Appointments() {
             } catch (error) {
                 console.error("Failed to fetch appointments:", error);
             } finally {
-                setLoading(false);
+                setIsLoadingWhileFetching(false);
             }
         };
         fetchAppointments();
     }, []);
 
-    if (loading) {
-        return <div className="relative bg-white rounded-xl p-10">Loading...</div>;
+    const suspendAppointment = async (id: string) => {
+        setSelectedAppointmentId(id);
+        new Modal().showModal();
     }
 
-    if (!appointments || appointments.rowCount === 0) {
-        return <div className="relative bg-white rounded-xl p-10">No appointments found.</div>;
+    const handleSuspendConfirm = async () => {
+        try {
+            setIsLoadingWhileDeleting(true);
+            const response = await fetch(`/api/deleteAppointment?id=${selectedAppointmentId}`, { method: 'DELETE' })
+            if (!response.ok) {
+                throw new Error("Failed to delete the appointment from the database");
+            }
+
+            setAppointments((prevAppointments) => ({
+                ...prevAppointments!,
+                rows: prevAppointments!.rows.filter((appointment) => appointment.id !== selectedAppointmentId),
+                rowCount: prevAppointments!.rowCount - 1,
+            }));
+        } catch (error) {
+            console.error(error)
+        } finally {
+            setIsLoadingWhileDeleting(false);
+        }
+    }
+
+    if (isLoadingWhileFetching) {
+        return (
+            <div className="relative bg-white rounded-xl p-10">
+                <ButtonLoader />
+            </div>
+        );
     }
 
     return (
         <div className="relative bg-white rounded-xl flex justify-center items-center py-10 px-52">
-            <table className="table-auto">
+            <table className="table-auto" id="table">
                 <thead>
                     <tr className="space-x-10">
                         <th scope="col" className="px-6 py-3">
@@ -60,24 +92,39 @@ export default function Appointments() {
                         <th scope="col" className="px-6 py-3">
                             Hora
                         </th>
+                        <th scope="col" className="px-6 py-3">
+                            Suspender cita
+                        </th>
                     </tr>
                 </thead>
                 <tbody className="bg-white border-b">
-                    {appointments.rows.map((appointment, index) => (
-                        <tr key={index} className="bg-white border-b cursor-pointer hover:bg-gray-100">
-                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-                                {appointment.appointment_reason}
-                            </th>
-                            <td className="px-6 py-4">
-                                {new Date(appointment.date!).toLocaleDateString()}
-                            </td>
-                            <td className="px-6 py-4">
-                                {appointment.hour}
+                    {isLoadingWhileDeleting ? (
+                        <tr>
+                            <td colSpan={4} className="text-center py-4">
+                                <ButtonLoader />
                             </td>
                         </tr>
-                    ))}
+                    ) : appointments && appointments.rowCount !== 0 ? (
+                        appointments!.rows.map((appointment, index) => (
+                            <tr key={index} className="bg-white border-b cursor-pointer hover:bg-gray-100">
+                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                                    {appointment.appointment_reason}
+                                </th>
+                                <td className="px-6 py-4 text-center">
+                                    {new Date(appointment.date!).toLocaleDateString()}
+                                </td>
+                                <td className="px-6 py-4">
+                                    {appointment.hour}
+                                </td>
+                                <td className="px-6 py-4 flex justify-center items-center" onClick={() => suspendAppointment(appointment.id!)}>
+                                    <CiSquareRemove size={30} color="red" />
+                                </td>
+                            </tr>
+                        ))
+                    ) : <tr className="bg-white border-b cursor-pointer hover:bg-gray-100"><td colSpan={5} className="text-center">No tienes ninguna cita pendiente</td></tr>}
                 </tbody>
             </table>
+            <WarningAppointmentDelete handleSuspendConfirm={handleSuspendConfirm} />
         </div>
     );
 }
