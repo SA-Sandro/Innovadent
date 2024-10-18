f32602a0064388c60450ae3138b2960032b27aa2
Sandro.dev <sandrosuarezaranda@gmail.com>
2024-10-18 16:35:14 +0200

    Create a new file called "constants" that store all constants

---
+++ app/lib/constants.ts
@@ -0,0 +1,23 @@
+export const AVAILABLE_HOURS = [
+  "09:00:00",
+  "10:00:00",
+  "11:00:00",
+  "12:00:00",
+  "13:00:00",
+  "14:00:00",
+  "15:00:00",
+  "16:00:00",
+  "17:00:00",
+];
+
+export const ACCEPTED_FILE_TYPES = [
+  "image/png",
+  "image/jpg",
+  "image/jpeg",
+  "image/webp",
+];
+
+export const MAX_UPLOAD_SIZE = 1024 * 1024 * 3;
+export const REGEX = new RegExp(
+  "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)[A-Za-z\\d]{5,20}$"
+);
