c6c787a5eefb60ba414053445f44367fe3fa09a5
Sandro.dev <sandrosuarezaranda@gmail.com>
2024-10-22 18:40:45 +0200

    Create an endpoint which communicate with getAllNotPendingAppointments function

---
+++ pages/api/getAllNotPendingAppointments.ts
@@ -0,0 +1,21 @@
+import { getAllNotPendingAppointments } from "@/lib/data";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "GET") {
+    try {
+      console.log('hola')
+      const allNotPendingAppointments = await getAllNotPendingAppointments();
+      return res.status(200).json(allNotPendingAppointments);
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: "Failed to fetch the non-pending appointments" });
+    }
+  } else {
+    res.status(405).json({ error: "Method not allowed" });
+  }
+}
