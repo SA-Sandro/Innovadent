9c7d84c8a8b1804e8cf420326b93e355e6f80ab9
Sandro.dev <sandrosuarezaranda@gmail.com>
2024-10-22 19:04:17 +0200

    Add 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+function NotFoundPage() {
+    return (
+        <div className="relative bg-white p-20 rounded-xl">
+            <h1>Page not found</h1>
+        </div>
+    )
+}
+
+export default NotFoundPage
