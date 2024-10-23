1bf20d74e1c60e4f0e65e0cf1f57eecddcf00f8e
Sandro.dev <sandrosuarezaranda@gmail.com>
2024-10-23 11:39:57 +0200

    Improve login design

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,15 +2,13 @@ import Form from "@/ui/login/Form";
 
 export default async function Login() {
   return (
-    <>
-      <div className="flex flex-col justify-center items-center relative h-[90%] ">
-        <div className="p-8 rounded-xl bg-gray-50 mx-4 sm:mx-2 shadow">
-          <h2 className="text-gray-800 text-center text-2xl font-bold">
-            Inicio de sesión
-          </h2>
-          <Form />
-        </div>
+    <div className="flex flex-col justify-center items-center relative">
+      <div className="px-8 py-16 rounded-xl bg-gray-50 mx-4 sm:mx-2 shadow">
+        <h2 className="text-[#1F2937<] text-center text-3xl font-bold">
+          Inicia la sesión
+        </h2>
+        <Form />
       </div>
-    </>
+    </div>
   );
 }
