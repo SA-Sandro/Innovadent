d9d99f0def7eac0ddd1e7ba600fe45724fd6474c
Sandro.dev <sandrosuarezaranda@gmail.com>
2024-10-24 22:17:03 +0200

    Implement 404 error page

--- app/access-denied/page.tsx
+++ app/access-denied/page.tsx
@@ -11,7 +11,7 @@ export default function AccessDenied() {
                 <h3 className="text-center text-pretty mt-2">No tienes los permisos necesarios para accedes a esta página</h3>
                 <p className="text-pretty text-center">Si crees que se trata de un error, intenta ponerte en contacto con algún administrador</p>
             </div>
-            <Link href={'/home'} type="button" className="mt-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+            <Link href={'/'} type="button" className="mt-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
                 Volver
             </Link>
             <Image className="absolute -top-5 right-1 xs:right-10 w-18 h-18 md:w-24 md:h-24" src={'/images/Access_denied.png'} alt="access denied logo" width={100} height={100} />
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,12 @@
+import Image from "next/image"
+
 function NotFoundPage() {
     return (
-        <div className="relative bg-white p-20 rounded-xl">
-            <h1>Page not found</h1>
+        <div className="relative bg-white p-20 rounded-xl flex flex-col justify-center items-center">
+            <Image src={'/images/404_error.webp'} height={200} width={200} className="rounded-full h-64 w-64 border border-black" alt="404 error image" />
+            <h1 className="text-lg text-center mt-5">
+                La página solicitada no existe o no se ha podido encontrar
+            </h1>
         </div>
     )
 }
